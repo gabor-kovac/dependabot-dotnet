449753b00352c5ef683d7cfacf50e1c6723add5a
gabor-kovac <gabor.kovac@ametek.com>
2025-03-26 14:37:13 +0100

    Check vulnerable dependencies script

---
+++ check-vulnerable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Check dotnet packages for vulnerabilities, includes transitive packages
+
+dotnet restore
+
+dotnet list package --vulnerable --include-transitive --format json > /tmp/vulnerable_packages.json
+
+if jq -e '.. | objects | select(has("vulnerabilities"))' vulnerable_packages.json > /dev/null 2>&1; then
+    echo "Vulnerable packages detected:" >&2
+    jq '.. | objects | select(has("vulnerabilities")) | .vulnerabilities' vulnerable_packages.json >&2
+    exit 1
+else
+    echo "No vulnerable packages detected." >&1
+    exit 0
+fi
