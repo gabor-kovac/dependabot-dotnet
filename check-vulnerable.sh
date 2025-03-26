88f63a719fc7ef4a6610b1d4c4a43dfe06838d03
gabor-kovac <gabor.kovac@ametek.com>
2025-03-26 14:46:12 +0100

    Update check-vulnerable.sh

--- check-vulnerable.sh
+++ check-vulnerable.sh
@@ -5,9 +5,9 @@ dotnet restore
 
 dotnet list package --vulnerable --include-transitive --format json > /tmp/vulnerable_packages.json
 
-if jq -e '.. | objects | select(has("vulnerabilities"))' vulnerable_packages.json > /dev/null 2>&1; then
+if jq -e '.. | objects | select(has("vulnerabilities"))' /tmp/vulnerable_packages.json > /dev/null 2>&1; then
     echo "Vulnerable packages detected:" >&2
-    jq '.. | objects | select(has("vulnerabilities")) | .vulnerabilities' vulnerable_packages.json >&2
+    jq '.. | objects | select(has("vulnerabilities")) | .vulnerabilities' /tmp/vulnerable_packages.json >&2
     exit 1
 else
     echo "No vulnerable packages detected." >&1
