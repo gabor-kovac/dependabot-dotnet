b1adfd38619944689136d518e62d3d50f03c6066
gabor-kovac <gabor.kovac@ametek.com>
2025-03-27 10:38:11 +0100

    Format output

--- check-vulnerable.sh
+++ check-vulnerable.sh
@@ -11,7 +11,8 @@ trivy fs --scanners vuln --quiet --format json --skip-files "**/*.deps.json" --e
 
 if [ $? -eq 1 ]; then
     echo -e "\nVulnerable packages detected, pull request is blocked form merging!" >&2
-    trivy fs --scanners vuln --quiet --format table --skip-files "**/*.deps.json" "$SOURCE_PATH" | tee "$VULN_TABLE_PATH"
+    trivy fs --scanners vuln --quiet --format table --dependency-tree --skip-files "**/*.deps.json" "$SOURCE_PATH"
+    jq -c '.Results[] | .Target as $TARGET | .Type as $TYPE | .Vulnerabilities[] | {Package: .PkgName, Installed: .InstalledVersion, "Fixed version": .FixedVersion, ID: .VulnerabilityID, Severity: .Severity, Title: .Title, Target: $TARGET, Type: $TYPE}' "$VULN_JSON_PATH" | jtbl --markdown > "$VULN_TABLE_PATH"
     echo "### Vulnerable Packages Report" >> $GITHUB_STEP_SUMMARY
     cat "$VULN_TABLE_PATH" >> $GITHUB_STEP_SUMMARY
     exit 1;
